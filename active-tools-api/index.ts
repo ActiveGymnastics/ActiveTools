d8426c8c9e201f1998739c4732fd1aa326ddfcbe
Myles Keeffe <11828103+myleskeeffe@users.noreply.github.com>
2021-04-03 15:11:46 +0800

    Setup Backend

--- active-tools-api/index.ts
+++ active-tools-api/index.ts
@@ -1,25 +1,46 @@
 import express = require('express');
 import bodyParser = require('body-parser');
-import cors = require ('cors');
+import cors = require('cors');
+import winston = require('winston');
 
-const app = express();
-const port = process.env.PORT || 8000;
+const app = express()
+const port = process.env.PORT || 8000
 
-var corsOptions = {
-    origin: "*"
+const db = require('./models/index')
+db.sequelize.sync()
+
+const corsOptions = {
+  origin: '*'
+}
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'erorr' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+})
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }))
 }
 
 // Allow requests from any host
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
 // Parse requests with application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-    res.json({message: "Active Tools", ver: "0.0.1"})
+app.get('/', (req, res) => {
+  res.json({ message: 'Active Tools', ver: '0.0.1' })
 })
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);})
+  console.log(`Server started on port ${port}`)
+})
